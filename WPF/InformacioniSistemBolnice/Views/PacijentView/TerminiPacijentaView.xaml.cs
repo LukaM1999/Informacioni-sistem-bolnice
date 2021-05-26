7861c18ac987218c12e63e6a24bd8d14eb4db4e8
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2021-05-26 12:41:32 +0200

    fix: Fixed binding property names in xaml of anamnesis view

--- WPF/InformacioniSistemBolnice/Views/PacijentView/TerminiPacijentaView.xaml.cs
+++ WPF/InformacioniSistemBolnice/Views/PacijentView/TerminiPacijentaView.xaml.cs
@@ -11,6 +11,8 @@ using System.Threading;
 using InformacioniSistemBolnice.Views.Pacijent;
 using System.Diagnostics;
 using InformacioniSistemBolnice.ViewModels.Pacijent;
+using InformacioniSistemBolnice.ViewModels.PacijentViewModel;
+using InformacioniSistemBolnice.Views.PacijentView;
 
 namespace InformacioniSistemBolnice
 {
@@ -60,7 +62,7 @@ namespace InformacioniSistemBolnice
 
         private void OtkaziTermin(object sender, RoutedEventArgs e)
         {
-            PacijentKontroler.Instance.Otkazivanje((Termin)listaZakazanihTermina.SelectedValue);
+            PacijentKontroler.Instance.OtkaziTermin((Termin)listaZakazanihTermina.SelectedValue);
         }
 
         private void OtvoriVesti(object sender, RoutedEventArgs e)
