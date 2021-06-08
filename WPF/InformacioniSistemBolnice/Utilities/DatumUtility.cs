1b1419b6b3204624d51f449687e70c835410feb9
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2021-06-08 16:52:45 +0200

    feat: Implemented patient therapy report

---
+++ WPF/InformacioniSistemBolnice/Utilities/DatumUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformacioniSistemBolnice.Utilities
+{
+    public class DatumUtility
+    {
+        public const string FormatDatumaKratak = "dd.MM.yyyy.";
+        public const string FormatDatumaDugacak = "dd.MM.yyyy. HH:mm";
+    }
+}
